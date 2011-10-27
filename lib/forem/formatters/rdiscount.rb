cae02c10a719b3ecc94cac3bed5068b46c365838
Ryan Bigg <radarlistener@gmail.com>
2011-10-27 20:16:16 +1100

    There is no more Base class in Forem to inherit from

--- lib/forem/formatters/rdiscount.rb
+++ lib/forem/formatters/rdiscount.rb
@@ -2,7 +2,7 @@ require 'forem/formatters/base'
 require 'rdiscount'
 module Forem
   module Formatters
-    class RDiscount < Base
+    class RDiscount
       def self.format(text)
         ::RDiscount.new(ERB::Util.h(text)).to_html.html_safe
       end
