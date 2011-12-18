84470d4929f7296e602c97391f1479c7584be37c
Ryan Bigg <radarlistener@gmail.com>
2011-12-18 11:09:02 +1030

    Add blockquote method for proper quoting

--- lib/forem/formatters/rdiscount.rb
+++ lib/forem/formatters/rdiscount.rb
@@ -5,6 +5,12 @@ module Forem
       def self.format(text)
         ::RDiscount.new(text).to_html.html_safe
       end
+
+      def self.blockquote(text)
+        text.split("\n").map do |line|
+          "> " + line
+        end.join("\n")
+      end
     end
   end
 end
